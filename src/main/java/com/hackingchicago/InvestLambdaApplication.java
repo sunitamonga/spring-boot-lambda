a93c9acd643b62391ea6dcf2870f36a2b2955726
Sunita Monga <sunita.monga@capitalone.com>
2017-02-18 21:59:00 -0600

    First commit

---
+++ src/main/java/com/hackingchicago/InvestLambdaApplication.java
@@ -0,0 +1,12 @@
+package com.hackingchicago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestLambdaApplication.class, args);
+	}
+}
